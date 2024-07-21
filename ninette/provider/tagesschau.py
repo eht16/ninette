2d2fe4130feb9cc84cde88e01197d3592050f69a
Enrico Tröger <enrico.troeger@uvena.de>
2024-07-21 23:13:12 +0200

    Rewrite Tagesschau alerter to replace removed API endpoint

--- ninette/provider/tagesschau.py
+++ ninette/provider/tagesschau.py
@@ -39,39 +39,61 @@ class TagesschauBreakingNewsProvider(ProviderBase):
 
     def _process_tagesschau_breaking_news(self):
         news = self._fetch_tagesschau_news_feed()
-        alert = self._process_tagesschau_news(news)
-        if alert:
+        alerts = self._process_tagesschau_news(news)
+        for alert in list(alerts):
             if self._alert_needs_to_be_processed(alert):
                 self._mark_alert_as_processed(alert)
-                return [alert]
             else:
                 self._logger.debug('Skipping already processed alert "%s"', alert.identifier)
+                alerts.remove(alert)
 
-        return None
+        return alerts or None
 
     def _fetch_tagesschau_news_feed(self):
         response = self._perform_http_request(self._api_url)
         return response.json()
 
     def _process_tagesschau_news(self, news):
-        if 'breakingNews' in news:
-            breaking_news = news['breakingNews']
-            alert_text = ALERT_TEXT.format(
-                identifier=breaking_news['id'],
-                title=breaking_news['headline'],
-                url=breaking_news['url'],
-                text=breaking_news['text'],
-                date=breaking_news['date'])
+        alerts_news = self._process_news_list(news.get('news', []))
+        alerts_regional = self._process_news_list(news.get('regional', []))
+        return alerts_news + alerts_regional
 
-            alert = self._factor_alert(
-                identifier=breaking_news['id'],
-                title=f'Tagesschau: {breaking_news["headline"]}',
-                alert_type='BreakingNews',
-                text=alert_text)
+    def _process_news_list(self, news):
+        alerts = []
+        for news_item in news:
+            if news_item['breakingNews']:
+                text = self._factor_breaking_news_text(news_item)
+                alert_text = ALERT_TEXT.format(
+                    identifier=news_item['sophoraId'],
+                    title=news_item['title'],
+                    url=news_item['detailsweb'],
+                    text=text,
+                    date=news_item['date'])
 
-            # attach the original json to the alert
-            alert.attach_original_event(news)
+                alert = self._factor_alert(
+                    identifier=news_item['sophoraId'],
+                    title=f'Tagesschau: {news_item["title"]}',
+                    alert_type='BreakingNews',
+                    text=alert_text)
 
-            return alert
+                # attach the original json to the alert
+                alert.attach_original_event(news_item)
 
-        return None
+                alerts.append(alert)
+
+        return alerts
+
+    def _factor_breaking_news_text(self, news_item):
+        news_text = []
+        for content_item in news_item.get('content', []):
+            if content_item['type'] == 'text':
+                plain_text = self._replace_html(content_item['value'])
+                news_text.append(plain_text)
+                news_text.append('\n')
+            elif content_item['type'] == 'headline':
+                news_text.append('\n')
+                plain_text = self._replace_html(content_item['value'])
+                news_text.append(plain_text)
+                news_text.append('\n')
+
+        return '\n'.join(news_text)
